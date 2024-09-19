4cb50a28431a533c2f71253bee746069b6985494
akem <singhakem03@gmail.com>
2024-09-19 23:52:40 +0530

    final for now

--- components/page/Intro.tsx
+++ components/page/Intro.tsx
@@ -1,6 +1,6 @@
 export default function Intro() {
-    return <div className="flex pt-24 flex-col justify-center bg-[#0A0A0A] ">
-        <div className="text-white ml-96 pl-11">
+    return <div className="flex pt-24 flex-col text-white justify-center bg-[#0A0A0A] ">
+        <div className=" ml-96 pl-11">
             <div>
             My name is 
             </div>
@@ -8,10 +8,13 @@ export default function Intro() {
         <div className="mt-2 flex justify-center">
             <div className="font-bold text-7xl  text-neutral-500">AKEMNOOR SINGH</div>
         </div>
-        <div className="mx-auto mt-10 text-white" >
+        <div className="mx-auto mt-10" >
             Engineering undergrad, currenlty in my final year. Specaializing in Electronics and Communication.
             <p className="font-bold">
             I am proficient in  NextJs and MERN.
+            </p>
+            <p className="mt-2">
+                I am looking for part-time or full time work.
             </p>
         </div>
     </div>
